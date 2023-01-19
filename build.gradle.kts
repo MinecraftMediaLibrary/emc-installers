f8dd011af57f55a99dfd2cd18be93d3b274dd2fe
BuildTools <unconfigured@null.spigotmc.org>
2023-01-18 23:24:01 -0500

    I'm an Idiot

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,9 +17,9 @@ dependencies {
         "com.google.guava:guava:31.1-jre",
         "uk.co.caprica:vlcj:4.8.2",
         "uk.co.caprica:vlcj-natives:4.8.1",
-        "net.java.dev.jna:jna:5.12.1",
-        "net.java.dev.jna:jna-platform:5.12.1",
-        "com.google.code.gson:gson:2.10"
+        "net.java.dev.jna:jna:5.13.0",
+        "net.java.dev.jna:jna-platform:5.13.0",
+        "com.google.code.gson:gson:2.10.1"
     ).forEach {
         compileOnlyApi(it)
         testRuntimeOnly(it)
@@ -81,6 +81,6 @@ subprojects {
     }
 
     task<Wrapper>("wrapper") {
-        gradleVersion = "7.5.1"
+        gradleVersion = "7.6"
     }
 }
